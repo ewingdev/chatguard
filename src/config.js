77c4c4865cf1fa3775839c566bada455303fee24
ewingdev <96184300+ewingdev@users.noreply.github.com>
2022-03-12 13:51:53 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const config = { 
 //------------Connections-----------//
-    Client_Token: 'OTEwOTkwMTk3Mjg0NTY5MDg4.YZa3vA.qOge5LzkKe5dn2oL04II70e2hEY',
-    MongoDB_ConnectURL: 'mongodb+srv://rootuser:ewingbaba@cluster0.tjarp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    Client_Token: 'TOKEN',
+    MongoDB_ConnectURL: 'MONGODBURL',
     //------------BotSettings-----------//
-    Prefix: '!',
-    BotOwners: ["774591026940739585"],
+    Prefix: 'PREFİX',
+    BotOwners: ["SAHİPİD"],
     Custom_Status_Text: 'ewing',
     Custom_Status_Type: 'PLAYING', // => PLAYING / WATCHING / LISTENING
     Custom_Status: 'dnd', // => dnd / idle / online / invisible
-    VoiceChannelID: '936299808921104454'
+    VoiceChannelID: 'SESKANAL'
 };
   
 module.exports = config;
